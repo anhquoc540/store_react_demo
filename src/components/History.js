5746c1d2cbddae0963fdee74b6ec65581ead82f7
Lam-Hoang-Gia-Huy <huylhgse173548fpt.edu.vn>
2023-11-24 19:34:12 +0700

    update history and detail order

--- src/components/History.js
+++ src/components/History.js
@@ -105,7 +105,7 @@ const HistoryOrders = () => {
 
     const interval = setInterval(() => {
       getHistoryOrders(userInfoDTO.id);
-    }, 2000); // Changed to 2 seconds as per your requirement
+    }, 1500); // Changed to 2 seconds as per your requirement
 
     // Clear the interval when the component is unmounted
     return () => clearInterval(interval);
@@ -116,7 +116,7 @@ const HistoryOrders = () => {
   for (let i = 0; i < state.length; i++) {
     // Format the total value as needed
     let formattedTotal =
-      new Intl.NumberFormat("en-US").format(state[i].total) + "VNĐ";
+      new Intl.NumberFormat("en-US").format(state[i].total) + "$";
 
     data1.push({
       orderCode: state[i].orderCode,
